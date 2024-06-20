94dac778888804004c0c0b8f8f6fac90ed55c7c2
box <1281956265@qq.com>
2024-06-20 12:04:25 +0800

    sync和Async添加日志

--- net/src/main/java/com/zfoo/net/router/Router.java
+++ net/src/main/java/com/zfoo/net/router/Router.java
@@ -216,8 +216,10 @@ public class Router implements IRouter {
         }
 
         // my服务器发送协议log
-        logger.info("server send, packetInfo={}, affachment={}, session={}",
-          packet, attachment, session);
+        if (packet.getClass() != Heartbeat.class) {
+            logger.info("server send, packetInfo={}, attachment={}, session={}",
+              packet, attachment, session);
+        }
 
         channel.writeAndFlush(packetInfo);
     }
@@ -254,6 +256,10 @@ public class Router implements IRouter {
             if (answerClass != null && answerClass != responsePacket.getClass()) {
                 throw new UnexpectedProtocolException("client expect protocol:[{}], but found protocol:[{}]", answerClass, responsePacket.getClass().getName());
             }
+
+            // my接收协议的日志
+            logger.info("Sync server receive，packetClazz={}, packetInfo={}",
+              responsePacket.getClass().getSimpleName(), responsePacket);
 
             @SuppressWarnings("unchecked")
             var syncAnswer = new SyncAnswer<>((T) responsePacket, clientSignalAttachment);
@@ -295,6 +301,10 @@ public class Router implements IRouter {
                         if (answer == null) {
                             throw new NetTimeOutException("async ask [{}] timeout exception", packet.getClass().getSimpleName());
                         }
+
+                        // my接收协议的日志
+                        logger.info("Async server receive，packetClazz={}, packetInfo={}",
+                          answer.getClass().getSimpleName(), answer);
 
                         if (answer.getClass() == Error.class) {
                             throw new ErrorResponseException((Error) answer);
